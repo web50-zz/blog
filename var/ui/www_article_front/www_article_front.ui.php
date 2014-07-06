32d784264b270f4008547c22aa970f9ca37e2569
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-07-06 18:54:21 +0400

    НОвые процедуры для получения  следующей и предыдущей попорядку  записи. Нескоьлко  параметров.  А таккже процедура  транк меню с учетом текущего поста в бредскрумбе

--- var/ui/www_article_front/www_article_front.ui.php
+++ var/ui/www_article_front/www_article_front.ui.php
@@ -31,8 +31,15 @@ class ui_www_article_front extends user_interface
 		{
 			return $this->list_by_tag();
 		}
-		$di = data_interface::get_instance('www_article_url_indexer');
-		$res = $di->search_by_uri('/'.SRCH_URI);
+		if(!$this->location)
+		{
+			$di = data_interface::get_instance('www_article_url_indexer');
+			$res = $di->search_by_uri('/'.SRCH_URI);
+		}
+		else
+		{
+			$res = $this->location;
+		}
 		if($res['item_id']>0)
 		{
 			return $this->get_item($res['item_id']);
@@ -43,6 +50,13 @@ class ui_www_article_front extends user_interface
 			return $this->get_post_list($res['category_id']);
 		}
 		return 'Not found';
+	}
+
+	public function pub_locator()
+	{
+		$di = data_interface::get_instance('www_article_url_indexer');
+		$res = $di->search_by_uri('/'.SRCH_URI);
+		$this->location = $res;
 	}
 
 	//9*  вывод списка постов  по входному   тэгу
@@ -79,6 +93,7 @@ class ui_www_article_front extends user_interface
 		$limit = $this->get_args('limit',5);
 		$post_type = $this->get_args('post_type',1);
 		$post_tmpl = $this->get_args('list_template','list.html');
+		$enable_pager = $this->get_args('enable_pager',false);
 		$page = request::get('page', 1);
 		$this->args['srch'] = array(
 			'sort'=>'release_date',
@@ -89,7 +104,7 @@ class ui_www_article_front extends user_interface
 		);
 		if($id>0)
 		{
-			$this->args['category_id'] = $id;
+			$this->args['srch']['category_id'] = $id;
 		}
 		$this->prepare_search();
 		$data = $di->get_list_by_srch($this->args['srch']);
@@ -151,6 +166,13 @@ class ui_www_article_front extends user_interface
 		$template = $this->get_args('post_template','item.html');
 		$di =  data_interface::get_instance('www_article_indexer');
 		$data = $di->get_record($id);
+		if($this->args['prev_next_post'])
+		{
+			$this->set_args(array('current'=>$id),true);
+			$prev_next = $this->pub_get_prev_next();
+			$data->prev_uri = $prev_next[0];
+			$data->next_uri = $prev_next[1];
+		}
 		return $this->parse_tmpl($template,$data);
 	}
 
@@ -182,6 +204,52 @@ class ui_www_article_front extends user_interface
 			$this->args['srch']['s'] = $string;
 		}
 	}
-		
+
+	public function pub_get_prev_next()
+	{
+		$post_type = $this->get_args('post_type',1);
+		$current_post = $this->get_args('current','0');
+		$di = data_interface::get_instance('www_article_indexer');
+		$di->push_args(array('_spost_type'=>$post_type));
+		$di->set_order('release_date','desc');
+		$res = $di->_get()->get_results();
+		$di->pop_args();
+		foreach($res as $key=>$value)
+		{
+			if($value->item_id == $current_post)
+			{
+				$prev_o = $res[$key+1];
+				$next_o = $res[$key-1];
+				if($prev_o)
+				{
+					$prev = $prev_o->uri;
+				}
+				if($next_o)
+				{
+					$next = $next_o->uri;
+				}
+
+			}
+		}
+		return array($prev,$next);
+	}
+
+
+	public function pub_trunc()
+	{
+		$st = data_interface::get_instance('structure');
+		$data = $st->get_trunc_menu();
+		if($this->location['item_id']>0)
+		{
+			$di = data_interface::get_instance('www_article_indexer');
+			$di->_flush();
+			$di->push_args(array(
+				'_sitem_id'=>$this->location['item_id'],
+			));
+			$res = $di->_get()->get_results(0);
+			$data[] = array('title'=>$res->title,'name'=>$res->uri,'uri'=>'/'.$res->uri.'/');
+		}
+		return $this->parse_tmpl('trunc.html',$data);
+	}
 }
 ?>
