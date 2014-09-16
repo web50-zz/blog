616f585e53f754ea60f8bf053a730b4c30b35982
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-09-16 17:01:33 +0400

    Параметры PAGE_URI SRCH_URI args  передаются теперь  шаблонв  в get_list_parametric www_article_front

--- var/ui/www_article_front/www_article_front.ui.php
+++ var/ui/www_article_front/www_article_front.ui.php
@@ -148,6 +148,9 @@ class ui_www_article_front extends user_interface
 			$st->collect_resources($pager,'pager');
 			$data['pager'] =$pager->get_pager(array('page' => $page, 'total' => $data['total'], 'limit' => $limit, 'prefix' => $_SERVER['QUERY_STRING']));
 		}
+		$data['args'] = $this->args;
+		$data['PAGE_URI'] = PAGE_URI;
+		$data['SRHC_URI'] = SRCH_URI;
 		return $this->parse_tmpl($template,$data);
 	}
 
