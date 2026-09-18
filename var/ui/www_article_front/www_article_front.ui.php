2c6a8fe19a31e3349e455529a04ad76c94832e87
root <root@srosss.diesel.loc>
2026-09-18 20:00:19 +0300

    Revert "feat: www_article_front — BreadcrumbList + Article JSON-LD, guarded (#026)"
    This reverts commit 71e2a15736d169404eb5403896141e7f63f442c6.

--- var/ui/www_article_front/www_article_front.ui.php
+++ var/ui/www_article_front/www_article_front.ui.php
@@ -329,11 +329,6 @@ class ui_www_article_front extends user_interface
 			$st->add_body_class($item_body_class);
 		}
 
-		$site = registry::get('SITE_URL');
-		$data->jsonld_article = !empty($this->location['item_id']) && $site && class_exists('lib_jsonld')
-			? lib_jsonld::article((array)$data, $site)
-			: '';
-
 		return $this->parse_tmpl($template,$data);
 	}
 
@@ -437,8 +432,7 @@ class ui_www_article_front extends user_interface
 			$res = $di->_get()->get_results(0);
 			$data[] = array('title'=>$res->title,'name'=>$res->uri,'uri'=>'/'.$res->uri.'/');
 		}
-		$site = registry::get('SITE_URL');
-		return ($site && class_exists('lib_jsonld') ? lib_jsonld::breadcrumb_list($data, $site) : '') . $this->parse_tmpl('trunc.html',$data);
+		return $this->parse_tmpl('trunc.html',$data);
 	}
 	/* 9* 12022016 Выводит список  публикаций входящих в подкатеггории указанной категории  сгруппированный по подкатегориям */
 	public function pub_sub_list_by_category()
